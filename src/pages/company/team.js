aadd27ea77288ed2436de227ee38f8373d612659
eneax <19818937+eneax@users.noreply.github.com>
2018-12-01 11:16:46 +0100

    Create team page

---
+++ src/pages/company/team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/layout'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptatum totam asperiores architecto sed veniam blanditiis officiis quas qui saepe odio, magnam optio amet, libero hic commodi delectus impedit fuga?</p>
+    </div>
+  </Layout>
+)
+
+export default Team
