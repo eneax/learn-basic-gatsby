c4172e03e0c0c86aed0fdc1254e2878715b657ef
eneax <19818937+eneax@users.noreply.github.com>
2018-12-01 11:11:51 +0100

    Create about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsum doloribus iure culpa iste velit, ut saepe quo facilis laboriosam eum dolor animi inventore quisquam in? Nisi ducimus incidunt illum.</p>
+    </div>
+  </Layout>
+)
+
+export default About
