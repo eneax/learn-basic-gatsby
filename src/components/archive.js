4b75a3ed814c315a401958ea9116844e90bd711d
eneax <19818937+eneax@users.noreply.github.com>
2018-12-02 15:10:49 +0100

    Refactor archive.js

--- src/components/archive.js
+++ src/components/archive.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
-
-const Archive = () => (
-  <StaticQuery
-    query={graphql`
-      query BlogPostArchive {
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                title
-                path
-              }
-            }
+const POST_ARCHIVE_QUERY = graphql`
+  query BlogPostArchive {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
           }
         }
       }
-    `}
+    }
+  }
+`
+
+const Archive = () => (
+  <StaticQuery
+    query={POST_ARCHIVE_QUERY}
     render={({allMarkdownRemark}) => (
       <>
         <aside>
           <h3>Archive</h3>
-          {allMarkdownRemark.edges.map(edge => (
-            <li>{edge.node.frontmatter.title}</li>
-          ))}
+          <ul>
+            {allMarkdownRemark.edges.map(edge => (
+              <li key={edge.node.frontmatter.path}>
+                {edge.node.frontmatter.title}
+              </li>
+            ))}
+          </ul>
         </aside>
       </>
     )}
