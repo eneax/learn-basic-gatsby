6f5d40d7f4a71d660cf1a6c00804fab6367d2a69
eneax <19818937+eneax@users.noreply.github.com>
2018-12-04 19:18:35 +0100

    Style archive list

--- src/components/listing.js
+++ src/components/listing.js
@@ -56,7 +56,7 @@ const Listing = () => (
           </Link>
           <p>{node.frontmatter.date}</p>
           <p>{node.excerpt}</p>
-          <Link className="read-more" to={`/posts${node.frontmatter.slug}`}>Read More</Link>
+          <Link class="read-more" to={`/posts${node.frontmatter.slug}`}>Read More</Link>
         </Post>
       ))
     )}
